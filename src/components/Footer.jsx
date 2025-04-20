a72b112e15bcf8ee951d6a3b1a7cb33c34823922
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-04-19 22:26:43 -0600

    Remove container component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,18 @@
-import { Container } from './Container';
-
 const Footer = () => {
   return (
-    <footer className="mt-auto bg-white">
-      <Container.Outer>
+    <footer className="mt-auto bg-zinc-100">
+      <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="border-t border-zinc-100 pt-6 pb-10">
-          <Container.Inner>
+          <div className="mx-auto max-w-2xl lg:max-w-5xl">
             <div className="flex flex-col items-center justify-end sm:flex-row">
               <p className="text-sm text-zinc-500">
                 © {new Date().getFullYear()} Marave Bautista. All rights
                 reserved.
               </p>
             </div>
-          </Container.Inner>
+          </div>
         </div>
-      </Container.Outer>
+      </div>
     </footer>
   );
 };
