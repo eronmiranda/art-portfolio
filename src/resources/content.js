bdfd390594549b321850ad3ab61e99bb2f601b6f
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-04-30 00:41:13 -0600

    Rename about-image.png to avatar.png

--- src/resources/content.js
+++ src/resources/content.js
@@ -12,7 +12,7 @@ const about = {
   description: `Meet ${person.name} from ${person.location}`,
   avatar: {
     display: true,
-    src: "/src/assets/about-image.png",
+    src: "/src/assets/avatar.png",
     alt: `${person.name} avatar`,
   },
   altText: "A digital artwork of a character with a colorful background",
