d07e94f12cc61e6b0ed7451312db7ff3ea88d794
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-05-06 20:58:03 -0600

    make About page component easy to read
    simplify the use of display (this is no brainer). There's no need to pass the display prop to Avatar and Intro compoennt

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,29 +2,27 @@ import { useState } from "react";
 import { about } from "../resources/content";
 import { motion } from "motion/react";
 
-function Avatar({ src, display, onLoad, loaded }) {
-  if (!display) return null;
+function Avatar({ src }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <div className="flex flex-shrink-0 justify-center md:pl-8 lg:justify-end lg:pl-15">
       <div className="max-w-xs px-2.5 lg:max-w-md">
-        {!loaded && (
-          <div className="animate-pulse">
-            <div className="aspect-square h-full w-full rotate-3 rounded-2xl bg-gray-300 shadow-xl" />
-          </div>
+        {!imageLoaded && (
+          <div className="aspect-square rotate-3 animate-pulse rounded-2xl bg-gray-300 shadow-xl" />
         )}
         <motion.img
           src={src}
           alt=""
           initial={{ opacity: 0, scale: 1.1, filter: "blur(12px)" }}
           animate={
-            loaded
+            imageLoaded
               ? { opacity: 1, scale: 1, filter: "blur(0px)" }
               : { opacity: 0, scale: 1.1, filter: "blur(12px)" }
           }
           className="aspect-square h-auto w-full rotate-3 rounded-2xl object-cover shadow-xl"
           loading="lazy"
           decoding="async"
-          onLoad={onLoad}
+          onLoad={() => setImageLoaded(true)}
           fetchPriority="high"
         />
       </div>
@@ -32,13 +30,12 @@ function Avatar({ src, display, onLoad, loaded }) {
   );
 }
 
-function Intro({ display, headline, subline }) {
-  if (!display) return null;
+function Intro({ headline, subline }) {
   return (
-    <div className="flex-1">
-      <h1 className="text-4xl font-bold tracking-tight text-zinc-900 sm:text-5xl">
+    <div className="flex flex-col">
+      <h2 className="text-4xl font-bold tracking-tight text-zinc-900 sm:text-5xl">
         {headline}
-      </h1>
+      </h2>
       <div className="mt-6 space-y-7 text-base text-zinc-700">
         {subline.map((line, index) => (
           <p key={index}>{line}</p>
@@ -49,22 +46,17 @@ function Intro({ display, headline, subline }) {
 }
 
 function About() {
-  const [avatarLoaded, setAvatarLoaded] = useState(false);
-
+  const { avatar, intro } = about;
   return (
     <div className="mx-auto mt-16 max-w-7xl px-4 sm:mt-32 sm:px-6 lg:px-8">
       <div className="flex flex-col gap-y-16 md:flex-row-reverse md:items-start">
-        <Avatar
-          src={about.avatar.src}
-          display={about.avatar.display}
-          loaded={avatarLoaded}
-          onLoad={() => setAvatarLoaded(true)}
-        />
-        <Intro
-          display={about.intro.display}
-          headline={about.intro.headline}
-          subline={about.intro.subline}
-        />
+        {avatar.display && ( <Avatar src={avatar.src} /> )}
+        {intro.display && (
+          <Intro
+            headline={intro.headline}
+            subline={intro.subline}
+          />
+        )}
       </div>
     </div>
   );
