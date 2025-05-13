13060697e62ae64299ff1793705fc84244b831ee
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-05-13 17:31:37 -0600

    Modal: lessen backdrop darkness

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,7 +13,7 @@ function Modal({ onBackdropClick, children }) {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60"
       onClick={onBackdropClick}
     >
       {children}
