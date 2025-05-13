a39a2e66a52467d6cfb889444911da5ffc1d7b28
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-05-12 23:59:16 -0600

    ImageOverlay: allow custom styles

--- src/components/ImageOverlay.jsx
+++ src/components/ImageOverlay.jsx
@@ -1,8 +1,14 @@
-function ImageOverlay({ children, text }) {
+import { cx } from "../lib/utils";
+function ImageOverlay({ className, text, children }) {
   return (
     <div>
       {children}
-      <div className="absolute right-0 bottom-0 left-0 bg-white/70 p-2 opacity-0 backdrop-blur-sm transition-opacity group-hover:opacity-100 md:p-4">
+      <div
+        className={cx(
+          "absolute right-0 bottom-0 left-0 bg-white/70 p-2 opacity-0 backdrop-blur-sm transition-opacity group-hover:opacity-100 md:p-4",
+          className
+        )}
+      >
         <h4 className="text-xs font-light text-zinc-900 md:text-lg">{text}</h4>
       </div>
     </div>
