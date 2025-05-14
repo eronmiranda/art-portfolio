7b4450d0c22643b70cc8a0abe045fc7e4a19ed37
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-05-14 15:49:00 -0600

    Extract Navigation Bar from Header component
    the goal here is to simplify the header component for readability and maintainability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
-import { Link, useLocation } from "react-router-dom";
-import { routes } from "../resources/config";
-import { MotionDiv, MotionSpan } from "./Motion";
-import { cx } from "../lib/utils";
+import { Link } from "react-router-dom";
+import { MotionDiv } from "./Motion";
 import LazyImage from "./LazyImage";
+import NavBar from "./NavBar";
 import ThemeToggle from "./ThemeToggle";
 
 function Header() {
-  const location = useLocation();
-
   return (
     <header className="sticky top-0 z-50 bg-zinc-100 backdrop-blur dark:bg-zinc-900">
       <div className="mb-2 h-16 pt-6">
@@ -31,46 +28,7 @@ function Header() {
             </div>
             {/* Navigation */}
             <div className="flex flex-1 justify-end md:justify-center">
-              <nav
-                className="pointer-events-auto md:block"
-                aria-label="Main navigation"
-              >
-                <ul className="md:text-md relative flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-                  {routes.map(
-                    ({ isEnabled, to, label }) =>
-                      isEnabled && (
-                        <li key={to} className="relative">
-                          <Link
-                            to={to}
-                            className={cx(
-                              "relative z-10 block px-3.5 py-2 transition",
-                              location.pathname === to
-                                ? "font-semibold text-teal-500"
-                                : "text-zinc-800 dark:text-zinc-100",
-                            )}
-                            aria-label={label}
-                            aria-current={
-                              location.pathname === to ? "page" : undefined
-                            }
-                          >
-                            {label}
-                            {location.pathname === to && (
-                              <MotionSpan
-                                className="absolute inset-x-1 -bottom-px h-px bg-linear-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0"
-                                layoutId="tab-active-underline"
-                                transition={{
-                                  type: "tween",
-                                  stiffness: 500,
-                                  damping: 30,
-                                }}
-                              />
-                            )}
-                          </Link>
-                        </li>
-                      ),
-                  )}
-                </ul>
-              </nav>
+              <NavBar />
             </div>
             {/* light/dark mode toggle */}
             <div className="flex flex-1 justify-end">
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import { routes } from "../resources/config";
+import { cx } from "../lib/utils";
+import { MotionSpan } from "./Motion";
+import { Link, useLocation } from "react-router-dom";
+
+function NavBar() {
+  const location = useLocation();
+  return (
+    <nav
+      className="pointer-events-auto md:block"
+      aria-label="Main navigation"
+    >
+      <ul className="md:text-md relative flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        {routes.map(
+          ({ isEnabled, to, label }) =>
+            isEnabled && (
+              <li key={to} className="relative">
+                <Link
+                  to={to}
+                  className={cx(
+                    "relative z-10 block px-3.5 py-2 transition",
+                    location.pathname === to
+                      ? "font-semibold text-teal-500"
+                      : "text-zinc-800 dark:text-zinc-100",
+                  )}
+                  aria-label={label}
+                  aria-current={
+                    location.pathname === to ? "page" : undefined
+                  }
+                >
+                  {label}
+                  {location.pathname === to && (
+                    <MotionSpan
+                      className="absolute inset-x-1 -bottom-px h-px bg-linear-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0"
+                      layoutId="tab-active-underline"
+                      transition={{
+                        type: "tween",
+                        stiffness: 500,
+                        damping: 30,
+                      }}
+                    />
+                  )}
+                </Link>
+              </li>
+            ),
+        )}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
