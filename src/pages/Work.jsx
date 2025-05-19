1c7632bea7675ce84492a0217d4ad951144d5d3d
Eron Miranda <49223433+eronmiranda@users.noreply.github.com>
2025-05-19 13:18:04 -0600

    Work: convert motion.div to tailwindCSS animation

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,4 @@
 import { useMemo, lazy, Suspense } from "react";
-import { MotionDiv, MotionPresence } from "../components/Motion";
 import useFirestore from "../hooks/useFirestore";
 import SkeletonGallery from "../components/SkeletonGallery";
 import { filterAndMapImages } from "../lib/utils";
@@ -7,25 +6,15 @@ import { filterAndMapImages } from "../lib/utils";
 const Gallery = lazy(() => import("../components/Gallery"));
 
 function Work() {
-  const rawImages = useFirestore("arts");
+  const rawImages = useFirestore("images");
 
   const images = useMemo(() => filterAndMapImages(rawImages), [rawImages]);
 
   return (
     <>
-      <MotionPresence mode="popLayout">
-        <MotionDiv
-          key="gallery-container"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <Suspense fallback={<SkeletonGallery />}>
-            <Gallery images={images}></Gallery>
-          </Suspense>
-        </MotionDiv>
-      </MotionPresence>
+      <Suspense fallback={<SkeletonGallery />}>
+        <Gallery images={images}></Gallery>
+      </Suspense>
     </>
   );
 }
